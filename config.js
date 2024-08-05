ac502599702610986cd91fd7d8f549258511f765
TheDarkk5 <90706145+TheDarkk5@users.noreply.github.com>
2024-08-05 03:02:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     app: {
-        px: '+',
-        token: '',
-        owners: '843204602686078976',
+        px: '*',
+        token: 'MTEzNDYxMjkyODExMzQxNDI0NQ.G5e2u4.zSsukeegNojqd9rOPzoOwVWo4DAztPzi7KBK04',
+        owners: '303167802376978442',
         funny: '843204602686078976',
         color: '#FF0000',
         footer: 'sltcv ?',
