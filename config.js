480adb5c3c0a6cdb3ac9d653deaa06788da44b23
TheDarkk5 <90706145+TheDarkk5@users.noreply.github.com>
2024-08-05 13:24:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
     app: {
         px: '*',
-        token: 'MTEzNDYxMjkyODExMzQxNDI0NQ.G5e2u4.zSsukeegNojqd9rOPzoOwVWo4DAztPzi7KBK04',
         owners: '303167802376978442',
         funny: '843204602686078976',
         color: '#FF0000',
